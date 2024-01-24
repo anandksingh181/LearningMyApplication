04cde360c2623a86e65c8f790f86c42db1fdd76f
anandksingh181 <142203756+anandksingh181@users.noreply.github.com>
2024-01-24 15:22:22 +0530

    Navigate_from_one_Activity_to_anthor

--- app/src/main/java/com/example/learningmyapplication/MainActivity.kt
+++ app/src/main/java/com/example/learningmyapplication/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.learningmyapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnShowToast = findViewById<Button>(R.id.btnShowToast)
-
+        val btnSendMsgToNextActivity = findViewById<Button>(R.id.btnSendMsgToNextActivity)
+        val etUserMessage = findViewById<EditText>(R.id.etUserMessage)
         btnShowToast.setOnClickListener {
             Log.i("MainActivity","Button was clicked !")
-            Toast.makeText(this,"Button was clicked!",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"Button was clicked !",Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message : String = etUserMessage.text.toString()
+            Toast.makeText(this , message,Toast.LENGTH_SHORT).show()
+
+            val Intent = Intent(this,SecondActivity::class.java)
+            startActivity(Intent)
         }
     }
 }
